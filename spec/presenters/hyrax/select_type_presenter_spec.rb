163856d7c2a12a977b6355b1ddb7243fcdd831b7
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-08-15 13:14:28 -0700

    Work form should display selected collections based on params
    This passes on values given to the "my" controller (from the collection "Add Works" button), which are currently lost when selecting a work type. Adds a new method to the work form that selects one or more collections (based on the IDs passed in params) automatically in the Relationships tab of the rendered form. This is working for both single works and batches of works.
    
    Add method to presenter that adds the `add_works_to_collection` param to the new work/batch path only if it exists.
    
    Fixes samvera-labs/hyku#1374

--- spec/presenters/hyrax/select_type_presenter_spec.rb
+++ spec/presenters/hyrax/select_type_presenter_spec.rb
@@ -19,4 +19,42 @@ RSpec.describe Hyrax::SelectTypePresenter do
 
     it { is_expected.to eq 'Generic Work' }
   end
+
+  describe '#switch_to_new_work_path' do
+    subject { instance.switch_to_new_work_path(route_set: routes, params: params) }
+
+    context 'with add_works_to_collection param' do
+      let(:routes) { Rails.application.routes.url_helpers }
+      let(:params) { { add_works_to_collection: 'xyz123abc' } }
+
+      it { is_expected.to eq "/#{model.to_s.tableize}/new?add_works_to_collection=#{collection_id}" }
+    end
+
+    context 'with no params' do
+      let(:routes) { Rails.application.routes.url_helpers }
+      let(:params) { {} }
+
+      it { is_expected.to eq "/#{model.to_s.tableize}/new" }
+    end
+  end
+
+  describe '#switch_to_batch_upload_path' do
+    subject { instance.switch_to_batch_upload_path(route_set: routes, params: params) }
+
+    let(:collection_id) { 'xyz123abc' }
+
+    context 'with add_works_to_collection param' do
+      let(:routes) { Hyrax::Engine.routes.url_helpers }
+      let(:params) { { add_works_to_collection: collection_id } }
+
+      it { is_expected.to eq "/batch_uploads/new?add_works_to_collection=#{collection_id}&payload_concern=#{model}" }
+    end
+
+    context 'with no params' do
+      let(:routes) { Hyrax::Engine.routes.url_helpers }
+      let(:params) { {} }
+
+      it { is_expected.to eq "/batch_uploads/new?payload_concern=#{model}" }
+    end
+  end
 end
